9d89a368a4c8924e34b160e151b58b87d6f79482
poio90 <germanv09@gmail.com>
2020-06-21 19:09:39 -0300

    mas cambios

--- Contenidos/admin.py
+++ Contenidos/admin.py
@@ -2,7 +2,4 @@ from django.contrib import admin
 from .models import Documento #importamos el modelo  
 # Register your models here.
 
-class DocumentosAdmin(admin.ModelAdmin):
-    search_fields = ['created_date']
-
-admin.site.register(Documento,DocumentosAdmin)#lo registramos en el admin de django
+admin.site.register(Documento)#lo registramos en el admin de django
