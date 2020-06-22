d2d003f8b4a45c46f696079dd08d2e1eff49611a
poio90 <germanv09@gmail.com>
2020-06-22 15:05:58 -0300

    modelo User en el admin

--- Users/admin.py
+++ Users/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 from .models import User 
 # Register your models here.
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('last_name','first_name','tipo')
+    list_filter = ()
+    ordering = ['last_name','first_name']
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Info extra', {'fields': ('tipo',)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Info extra', {'fields': ('tipo',)}),
+    )
+
+admin.site.register(User,CustomUserAdmin)
--- Users/models.py
+++ Users/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
+    
     SELECT_TIPO = (('1','Lector'),('2','Editor'))      
     tipo = models.CharField('Tipo de usuario',max_length=20, choices=SELECT_TIPO)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email' , 'last_name', 'first_name']
+
+
 
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
