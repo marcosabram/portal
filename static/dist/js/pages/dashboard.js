d144a9acfee3c71193a4489c5c6617bde098c583
poio90 <germanv09@gmail.com>
2020-06-22 17:02:57 -0300

    select

--- static/dist/js/pages/dashboard.js
+++ static/dist/js/pages/dashboard.js
@@ -32,6 +32,8 @@ $(document).ready(function () {
     height: 120,
   })
 
+  $('#editable-select').editableSelect();
+
   // A few jQuery helpers for exporting only
   jQuery.fn.pop = [].pop;
   jQuery.fn.shift = [].shift;
